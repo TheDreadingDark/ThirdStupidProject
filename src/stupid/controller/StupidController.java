9bec1879b96e1ebe58333ba4aac2ff622861062d
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-09-07 13:53:10 -0600

    Added way to conversate with self.

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -6,5 +6,17 @@ public class StupidController
 	public void start()
 	{
 		System.out.println("I like to play videogames");
+		
+		testScanner();
 	}
+	
+	private void testScanner()
+	{
+		Scanner firstScanner;
+		firstScanner = new Scanner(System.in);
+		System.out.println("what is your favorite food?");
+		String answer = firstScanner.nextLine();
+		System.out.println("Oh cool, you like " + answer);
+	}
+	
 }
